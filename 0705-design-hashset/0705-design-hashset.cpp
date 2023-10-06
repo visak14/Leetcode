541ef14d58c18e60c64baf11f3282b7d20a4cf4b
visak14 <121089328+visak14@users.noreply.github.com>
2023-10-06 16:59:43 +0530

    Time: 323 ms (9.11%), Space: 40.3 MB (91.44%) - LeetHub

---
+++ 0705-design-hashset/0705-design-hashset.cpp
@@ -0,0 +1,45 @@
+class MyHashSet {
+public:
+     ListNode* head ;
+    MyHashSet() {
+      head = new ListNode(-1);
+    }
+    
+    void add(int key) {
+      ListNode* temp = head;
+        while(temp->next != NULL){
+            if(temp->next->val == key) return;
+            temp = temp->next;
+        }
+        ListNode* newnode = new ListNode(key);
+        temp->next = newnode;
+    }
+    
+    void remove(int key) {
+        ListNode * temp = head;
+        while (temp->next != NULL){
+            if (temp->next->val == key){
+                temp->next = temp->next->next;
+                break;
+           }
+            temp = temp->next;
+        }
+    }
+    
+    bool contains(int key) {
+     ListNode * temp = head->next;
+        while (temp != NULL){
+            if (temp->val == key)  return true;
+            temp = temp->next;
+        }
+        return false;
+    }
+};
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet* obj = new MyHashSet();
+ * obj->add(key);
+ * obj->remove(key);
+ * bool param_3 = obj->contains(key);
+ */
