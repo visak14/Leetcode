ad1283f16f99296e965a1bcac62ce8efaeae9d41
visak14 <121089328+visak14@users.noreply.github.com>
2023-10-08 18:51:55 +0530

    Time: 0 ms (100.00%), Space: 8.2 MB (18.07%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,30 +9,22 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode* slow = head;
+        ListNode *slow = head;
         ListNode* fast = head;
-        while (fast != NULL && fast->next != NULL){
+        while (fast != NULL  && fast->next != NULL){
             slow = slow->next;
             fast = fast->next->next;
-           
-            if(slow==fast)
-                break;        //if slow and fast meets then break out of the loop
+            if (slow == fast)
+                break;
         }
-		
-	    //if slow and fast didn't meet and we came out beacuse of no loop condition then return NULL
-        if(fast==NULL||fast->next==NULL)
+         if(fast==NULL||fast->next==NULL)
             return NULL;
-			
-		/*now place any pointer out of slow and fast at the beginning of the list and iterate over the list
-		again by moving both the pointers by one position at a time until they meet again. This is the
-		starting point of the loop, return it*/
-		
-       slow=head;
-        while(slow!=fast){
-            slow=slow->next;
-            fast=fast->next;
+        
+        slow = head;
+        while (slow!= fast){
+            slow = slow->next;
+            fast = fast->next;
         }
-		
-        return slow;           
+        return slow;
     }
 };
