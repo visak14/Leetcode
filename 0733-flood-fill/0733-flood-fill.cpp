3b7a1c2dec3d3b6a2f5a629c44a46e33a4bd18ab
visak14 <121089328+visak14@users.noreply.github.com>
2023-11-10 21:54:09 +0530

    Time: 3 ms (96.05%), Space: 14.3 MB (78.86%) - LeetHub

---
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    bool isvalid(vector<vector<int>>& image,int i, int j, int m ,int n, int color){
+        if (i>=0 && i<n && j >=0 && j<m && image[i][j] == color )
+            return true;
+        return false;
+    }
+    
+    void floodFillRec(vector<vector<int>>& image,int i, int j, int m ,int n, int color, int newcolor){
+        
+        image[i][j] = newcolor;
+        if(isvalid(image,i+1,j,m,n,color)){
+            floodFillRec(image, i+1, j, m ,n, color,newcolor);
+        }
+        if(isvalid(image,i-1,j,m,n,color)){
+            floodFillRec(image,i-1, j, m ,n, color,newcolor);
+        }
+        if(isvalid(image,i,j+1,m,n,color)){
+            floodFillRec(image,i, j+1, m ,n, color,newcolor);
+        }
+        if(isvalid(image,i,j-1,m,n,color)){
+            floodFillRec(image,i, j-1, m ,n, color,newcolor);
+        }
+    }
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int newcolor)
+    {
+        int n = image.size();
+        int m = image[0].size();
+           int color = image[sr][sc];
+                if (color == newcolor)
+                    return image;
+        floodFillRec(image , sr ,sc , m, n, color, newcolor);
+        return image;
+    }
+};
